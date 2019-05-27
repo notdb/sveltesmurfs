46ce7598631ce0376e017c02b743d8645e78c8bc
Doug <me@douglasbowen.com>
2019-05-27 17:13:07 -0400

    add the ability to post

--- sveltesmurfs/src/routes/AddNewSmurf.svelte
+++ sveltesmurfs/src/routes/AddNewSmurf.svelte
@@ -1,5 +1,5 @@
 <script>
-export let addsmurf;
+
 let newSmurf = {
 name: 'name',
 age: 'age',
@@ -7,8 +7,26 @@ height: 'height'
 }
 
 function testFun(smurf) {
-addsmurf(newSmurf, event);
+addSmurf(newSmurf, event);
 }
+
+  function postRequest(url, data) {
+    return fetch(url, {
+      credentials: "same-origin", // 'include', default: 'omit'
+      method: "POST", // 'GET', 'PUT', 'DELETE', etc.
+      body: JSON.stringify(data), // Coordinate the body type with 'Content-Type'
+      headers: new Headers({
+        "Content-Type": "application/json"
+      })
+    }).then(response => response.json());
+  }
+
+function addSmurf(smurf, event) {
+    event.preventDefault();
+    postRequest('http://localhost:3333/smurfs', smurf)
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+  };
 
 </script>
 
